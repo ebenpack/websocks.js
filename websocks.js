671ae7bb359202dbb2ed36c01907f030a7e3f6e3
ebenpack <ebenpackwood@gmail.com>
2015-12-23 18:04:00 -0500

    Write header on upgrade

--- websocks.js
+++ websocks.js
@@ -1,5 +1,12 @@
 function websocks(server) {
-
+    server.on('upgrade', function() {
+        socket.write(
+            'HTTP/1.1 101 Switching Protocols\r\n' +
+            'Upgrade: websocket\r\n' +
+            'Connection: Upgrade\r\n' +
+            "\r\n"
+        );
+    });
 }
 
 module.exports = websocks;
