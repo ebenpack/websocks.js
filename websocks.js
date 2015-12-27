2085800c55530bbfbed7ebbc451b4f288124ea4f
ebenpack <ebenpackwood@gmail.com>
2015-12-27 18:04:00 -0500

    Return Sec-WebSocket-Accept header

--- websocks.js
+++ websocks.js
@@ -1,9 +1,20 @@
+var crypto = require('crypto');
+
+function calculateSockKey(key) {
+    return (
+        crypto.createHash('sha1').update(
+            key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+        ).digest('base64')
+    );
+}
+
 function websocks(server) {
-    server.on('upgrade', function() {
+    server.on('upgrade', function(req, socket, head) {
         socket.write(
             'HTTP/1.1 101 Switching Protocols\r\n' +
             'Upgrade: websocket\r\n' +
             'Connection: Upgrade\r\n' +
+            "Sec-WebSocket-Accept: " + calculateSockKey(req.headers['sec-websocket-key'] + "\r\n") +
             "\r\n"
         );
     });
